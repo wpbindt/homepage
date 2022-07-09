06999cf7936fe8e0be0e1b107da3677284d95a3e
wpbindt <wesselbindt@gmail.com>
2022-07-10 01:07:48 +0200

    Bla

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,6 +4,7 @@ echo "$VAULT_PASS" > ~/.vault_pass.txt
 mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
+ssh -t wessel@109.237.219.178 "echo hi"
 
 ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/inventory.ini ansible/site.yml
 
