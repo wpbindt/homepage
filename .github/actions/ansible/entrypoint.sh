bc72b71433d33b0636a9b35ed8d099a4edf3491a
wpbindt <wesselbindt@gmail.com>
2022-07-10 01:17:20 +0200

    Bla

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -3,9 +3,9 @@
 echo "$VAULT_PASS" > ~/.vault_pass.txt
 mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
-echo ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
-echo ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa
 ssh  -o "StrictHostKeyChecking=no" wessel@109.237.219.178 
 
 
