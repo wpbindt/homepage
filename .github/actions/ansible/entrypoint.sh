8b7bea71355fe60b06c4eae04266f48b1a6c9328
wpbindt <wesselbindt@gmail.com>
2022-07-09 23:42:01 +0200

    Fix deploy script

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,5 +5,5 @@ mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/inventory.yml ansible/site.yml
+ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/inventory.ini ansible/site.yml
 
