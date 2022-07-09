334ac9a982363186fc244835d8f72894ffd5691f
wpbindt <wesselbindt@gmail.com>
2022-07-10 01:21:23 +0200

    Bla

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -6,7 +6,7 @@ ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh_key.txt.s
 cat ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa
-ssh  -o "StrictHostKeyChecking=no" wessel@109.237.219.178 
+ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" wessel@109.237.219.178 
 
 
 ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/inventory.ini ansible/site.yml
