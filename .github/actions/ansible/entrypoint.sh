f2c01398af54b556e83867fce76f3ebe980cb27b
wpbindt <wesselbindt@gmail.com>
2022-07-09 23:44:50 +0200

    Add debug prints

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 echo "$VAULT_PASS" > ~/.vault_pass.txt
+echo $VAULT_PASS
 mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa
 
 ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/inventory.ini ansible/site.yml
 
