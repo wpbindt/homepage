0a5ae78a8e2cc0b671674fdfd0ec5b30f5a52433
wpbindt <wesselbindt@gmail.com>
2022-07-11 01:28:17 +0200

    Use unordered list in homepage

--- generator/generate_homepage.py
+++ generator/generate_homepage.py
@@ -5,16 +5,17 @@ BASE_URL = '109.237.219.178'
 
 def filename_to_anchor(subdirectory: str, filename: str) -> str:
     link_text = ' '.join(filename[:-len('.html')].split('-'))
-    return f'<a href="{BASE_URL}/{subdirectory}/{filename}">{link_text}</a>\n'
+    return f'<li><a href="{BASE_URL}/{subdirectory}/{filename}">{link_text}</a></li>\n'
 
 
 html = '<body>\n'
 html += '<h1>My homepage</h1>\n'
 
 for subdirectory in os.listdir('content'):
-    html += '<h2>subdirectory</h2>\n'
+    html += '<h2>subdirectory</h2>\n<ul>\n'
     for filename in os.listdir(f'content/{subdirectory}'):
         html += filename_to_anchor(subdirectory, filename)
+    html += '</ul>'
 
 html += '</body>'
 
