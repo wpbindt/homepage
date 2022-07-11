1c236d024709845e9ed6eb4a3bc424dbf7305e4a
wpbindt <wesselbindt@gmail.com>
2022-07-11 23:30:29 +0200

    Switch to relative links

--- generator/generate_homepage.py
+++ generator/generate_homepage.py
@@ -1,18 +1,16 @@
 import os
 
-BASE_URL = '109.237.219.178'
-
 
 def filename_to_anchor(subdirectory: str, filename: str) -> str:
-    link_text = ' '.join(filename[:-len('.html')].split('-'))
-    return f'<li><a href="http://{BASE_URL}/{subdirectory}/{filename}">{link_text}</a></li>\n'
+    link_text = (' '.join(filename[:-len('.html')].split('-'))).capitalize()
+    return f'<li><a href="/{subdirectory}/{filename}">{link_text}</a></li>\n'
 
 
 html = '<body>\n'
 html += '<h1>My homepage</h1>\n'
 
 for subdirectory in os.listdir('content'):
-    html += f'<h2>{subdirectory}</h2>\n<ul>\n'
+    html += f'<h2>{subdirectory.title()}</h2>\n<ul>\n'
     for filename in os.listdir(f'content/{subdirectory}'):
         html += filename_to_anchor(subdirectory, filename)
     html += '</ul>'
