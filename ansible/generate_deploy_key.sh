e73ca818116b301e5488ad1bba0d1e865029f21d
wpbindt <wesselbindt@gmail.com>
2022-07-12 12:41:19 +0200

    WIP

---
+++ ansible/generate_deploy_key.sh
@@ -0,0 +1,5 @@
+CURRENT_DATE=$(date --iso-8601)
+ssh-keygen -N "" -t rsa -q -C ansible-deploy-key-$CURRENT_DATE -f ssh_key.txt.secret
+ansible-vault encrypt --vault-password-file vault_pass.txt ssh_key.txt.secret
+mv ssh_key.txt.secret.pub ssh_key.pub.nocommit
+rm vault_pass.txt
