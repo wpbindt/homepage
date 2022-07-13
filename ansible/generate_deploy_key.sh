dd8b49eabf61990b8556ffa62d4a0f567b474868
wpbindt <wesselbindt@gmail.com>
2022-07-13 14:59:36 +0200

    Improve deploy key generation

--- ansible/generate_deploy_key.sh
+++ ansible/generate_deploy_key.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-stat vault_pass.txt
+stat vault_pass.txt >> /dev/null
 CURRENT_DATE=$(date --iso-8601)
-ssh-keygen -N "" -t rsa -q -C ansible-deploy-key-$CURRENT_DATE -f ssh_key.txt.secret
+ssh-keygen -N "" -t rsa -q -C ansible-deploy-key-$CURRENT_DATE -f ssh_key.txt.secret <<< y
 ansible-vault encrypt --vault-password-file vault_pass.txt ssh_key.txt.secret
 mv ssh_key.txt.secret.pub ssh_key.pub.nocommit
 rm vault_pass.txt
