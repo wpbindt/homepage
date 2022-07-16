5b3a58da6696d674f814c1a126fbb350520e996b
wpbindt <wesselbindt@gmail.com>
2022-07-16 16:06:19 +0200

    Move html files back to content

--- generator/generate.py
+++ generator/generate.py
@@ -14,12 +14,28 @@ def convert_markup_to_html(
 
 
 for subdirectory in os.listdir('content'):
-    print('HERE')
-    subprocess.call(['mkdir', '-p', f'content/{subdirectory}'])
-    for markup_file in os.listdir(f'content/{subdirectory}'):
-        print(markup_file)
+    subprocess.call(['mkdir', '-p', f'static/{subdirectory}'])
+    all_files = os.listdir(f'content/{subdirectory}')
+    markup_files = [
+        filename
+        for filename in all_files
+        if filename.endswith('mu')
+    ]
+    for markup_file in markup_files:
         output_filename = markup_file[:-len('.mu')] + '.html'
         convert_markup_to_html(
             input_file=f'content/{subdirectory}/{markup_file}',
             output_file=f'static/{subdirectory}/{output_filename}',
         )
+
+    html_files = [
+        filename
+        for filename in all_files
+        if filename.endswith('html')
+    ]
+    for html_file in html_files:
+        subprocess.call([
+            'cp',
+            f'content/{subdirectory}/{html_file}',
+            f'static/{subdirectory}/{html_file}',
+        ])
