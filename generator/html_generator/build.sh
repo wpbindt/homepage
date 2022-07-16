9dbb383e58cec8cdd2db5362b0ab93b920c1ae68
wpbindt <wesselbindt@gmail.com>
2022-07-16 15:18:11 +0200

    Make build script interactive for shell argument

--- generator/html_generator/build.sh
+++ generator/html_generator/build.sh
@@ -6,8 +6,12 @@ else
     ARGUMENT=$1
 fi
 
+if [ $ARGUMENT == "shell" ]; then
+    INTERACTIVE="-it"
+fi
+
 docker container run \
-    --rm \
+    --rm $INTERACTIVE \
     --volume $(pwd)/dockers/html-generator-builder/scripts:/scripts \
     --volume $(pwd)/generator/html_generator:/workdir \
     --name html-generator-builder-container \
