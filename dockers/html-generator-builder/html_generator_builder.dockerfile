7cbafc1e9425b45e24f1b71a0b7fd5a9c752e6fa
wpbindt <wesselbindt@gmail.com>
2022-07-19 20:37:31 +0200

    Add trivial test suite

--- dockers/html-generator-builder/html_generator_builder.dockerfile
+++ dockers/html-generator-builder/html_generator_builder.dockerfile
@@ -3,4 +3,6 @@ FROM haskell:9.2.3-slim
 VOLUME /scripts
 VOLUME /workdir
 
+RUN cabal update
+
 ENTRYPOINT ["/bin/bash", "/scripts/entrypoint.sh"]
--- dockers/html-generator-builder/scripts/entrypoint.sh
+++ dockers/html-generator-builder/scripts/entrypoint.sh
@@ -4,6 +4,9 @@ cd /workdir
 if [ $1 == "build" ]; then
     echo "Starting to build"
     /scripts/build.sh
+elif [ $1 == "test" ]; then
+    echo "Running the tests"
+    /scripts/test.sh
 elif [ $1 == "clean" ]; then
     echo "Cleaning up"
     /scripts/clean.sh
