e8dd9cf98898fcf529b04433065ed46bc2acbbd0
wpbindt <wesselbindt@gmail.com>
2022-07-22 12:55:10 +0200

    Refactor tests a bit

--- generator/html_generator/test/HtmlGeneratorSpec.hs
+++ generator/html_generator/test/HtmlGeneratorSpec.hs
@@ -3,34 +3,32 @@ module HtmlGeneratorSpec (spec) where
 import Test.Hspec
 import HtmlGenerator
 
-checkConversion :: String -> String -> String -> String -> Spec
-checkConversion message title markup html = it message $ (convert title markup) == html
+checkBodyConversion :: String -> String -> String -> Spec
+checkBodyConversion message markup body = it message $ 
+        (convert "Some title" markup) == 
+                ("<html><head><title>Some title</title></head><body>" <> body <> "</body></html>")
 
 emptyConvertSpec :: Spec
-emptyConvertSpec = checkConversion "Converts an empty string to an empty document"
-        "My title" ""
-        "<html><head><title>My title</title></head><body></body></html>"
+emptyConvertSpec = checkBodyConversion 
+        "Converts an empty string to an empty document"
+        "" ""
 
 convertParagraphSpec :: Spec
-convertParagraphSpec = checkConversion "Converts a paragraph to a paragraph"
-        "My title" "bla"
-        "<html><head><title>My title</title></head><body><p>bla\n</p></body></html>"
+convertParagraphSpec = checkBodyConversion "Converts a paragraph to a paragraph"
+        "bla" "<p>bla\n</p>"
 
 convertHeaderSpec :: Int -> Spec
-convertHeaderSpec w = checkConversion ("Check header of weight " <> wString)
-        "Some title" markup html
+convertHeaderSpec w = checkBodyConversion ("Check header of weight " <> wString)
+        markup body
             where wString = show w
                   markup = (take w $ repeat '*') <> "bla"
                   openTag = "<h" <> wString <> ">"
                   closeTag = "</h" <> wString <> ">"
-                  html = "<html><head><title>Some title</title></head><body>"
-                    <> openTag <> "bla" <> closeTag
-                    <> "</body></html>"
+                  body = openTag <> "bla" <> closeTag
 
 escapeCharacterSpec :: String -> String -> Spec
-escapeCharacterSpec input output = checkConversion "Converts a paragraph to a paragraph"
-        "My title" input
-        ("<html><head><title>My title</title></head><body><p>" <> output <> "\n</p></body></html>")
+escapeCharacterSpec input output = checkBodyConversion "Converts a paragraph to a paragraph"
+        input ("<p>" <> output <> "\n</p>")
 
 spec :: Spec
 spec = do
