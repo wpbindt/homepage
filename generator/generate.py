f8859d15e2869432a5387009168768517fdd09a3
wpbindt <wesselbindt@gmail.com>
2022-07-23 09:57:54 +0200

    Try to refactor python script

--- generator/generate.py
+++ generator/generate.py
@@ -1,68 +1,103 @@
-import os
+from pathlib import Path
+import shutil
 import subprocess
+from typing import List
 
 
-def convert_markup_to_html(
-    input_file: str,
-    output_file: str,
-) -> None:
+class StaticFile:
+    converter: Callable[[Path, Path], None]
+    file_path: Path
+
+    def write_static(self, target_dir: Path) -> None:
+        target_path = target_dir / self.target_filename
+        target_path.parent.mkdir()
+        self.converter(
+            self.file_path, 
+            target_path,
+        )
+
+    @property
+    def target_filename(self) -> Path:
+        return Path(*self.file_path.parts[-2:]).with_suffix('.html')
+    
+    @property
+    def subdir(self) -> str:
+        return self.file_path.parent.name
+    
+    @property
+    def title_text(self) -> str:
+        return ' '.join(self.file_path.stem.split('-')).capitalize()
+
+
+def write_markup_file(input_file: Path, output_file: Path) -> None:
     subprocess.call([
         'generator/html_generator/html-generator', 
-        input_file, 
-        output_file,
+        input_file,
+        output_file
     ])
 
 
-def generate_static_content() -> None:
-    for subdirectory in os.listdir('content'):
-        subprocess.call(['mkdir', '-p', f'static/{subdirectory}'])
-        all_files = os.listdir(f'content/{subdirectory}')
-        markup_files = [
-            filename
-            for filename in all_files
-            if filename.endswith('mu')
+def write_html_file(input_file: Path, output_file: Path) -> None:
+    shutil.copy(
+        input_file,
+        output_file,
+    )
+
+
+def to_file(path: Pathlib) -> StaticFile:
+    if path.is_dir() or path.suffix not in {'.html', '.mu'}:
+        raise ValueError
+    if path.suffix == '.html':
+        converter = write_html_file
+    else:
+        converter = write_markup_file
+    return StaticFile(converter, path)
+
+
+def get_parseables() -> Iterable[StaticFile]:
+    content_dir = Path('content')
+    return {
+        to_file(file_path)
+        for subdirectory in content_dir.iterdir()
+        for file_path in subdirectory.iterdir()
+        if subdirectory.is_dir()
+    }
+
+
+def get_index_html(files: Iterable[StaticFile]) -> List[str]:
+    html = [
+        '<html>', 
+        '<head>', 
+        '<title>My homepage</title>', 
+        '</head>',
+        '<body>',
+        '<h1>My homepage</h1>',
+    ]
+    subdirs = defaultdict(set)
+    for file in files:
+        subdirs[file.subdir].add(file)
+    for subdir, subfiles in subdirs.items():
+        subdir_html = [
+            f'<h2>{subdir.title()}</h2>',
+            '<ul>',
         ]
-        for markup_file in markup_files:
-            output_filename = markup_file[:-len('.mu')] + '.html'
-            convert_markup_to_html(
-                input_file=f'content/{subdirectory}/{markup_file}',
-                output_file=f'static/{subdirectory}/{output_filename}',
-            )
-
-        html_files = [
-            filename
-            for filename in all_files
-            if filename.endswith('html')
-        ]
-        for html_file in html_files:
-            subprocess.call([
-                'cp',
-                f'content/{subdirectory}/{html_file}',
-                f'static/{subdirectory}/{html_file}',
-            ])
+        for file in subfiles:
+            subdir_html.append(f'<li> <a href="{file.target_filename}">{file.title_text}</a></li>')
+        subdir_html.append('</ul>')
+        html.extend(subdir_html)
+    html.extend(['</body>', '</html>'])
+    return html
 
 
-def filename_to_anchor(subdirectory: str, filename: str) -> str:
-    link_text = (' '.join(filename[:-len('.html')].split('-'))).capitalize()
-    return f'<li><a href="/{subdirectory}/{filename}">{link_text}</a></li>\n'
-
-
-def generate_index() -> None:
-    html = '<body>\n'
-    html += '<h1>My homepage</h1>\n'
-
-    for subdirectory in os.listdir('static'):
-        html += f'<h2>{subdirectory.title()}</h2>\n<ul>\n'
-        for filename in os.listdir(f'static/{subdirectory}'):
-            html += filename_to_anchor(subdirectory, filename)
-        html += '</ul>'
-
-    html += '</body>'
-
-    with open('static/index.html', 'w') as f:
-        f.write(html)
+def main() -> None:
+    files = get_parseables()
+    target_dir = Path('static')
+    target_dir.mkdir()
+    for file in files:
+        file.write_static()
+    index_html = get_index_html(files)
+    (target_dir / 'index.html').write_text('\n'.join(index_html))
 
 
 if __name__ == '__main__':
-    generate_static_content()
-    generate_index()
+    main()
