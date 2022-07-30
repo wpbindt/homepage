f0a9aabe4573c428011187e568b9b8c39a57e92d
wpbindt <wesselbindt@gmail.com>
2022-07-30 19:52:28 +0200

    Add attoparsec-based HTML escaper

---
+++ html-generator/src/HtmlGenerator/HtmlEscapedText.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+module HtmlGenerator.HtmlEscapedText
+    ( escape
+    , printEscaped
+    , HTMLEscapedText
+    ) where
+
+import Control.Applicative
+import qualified Data.Text as T
+
+import Data.Attoparsec.Text
+
+
+data HTMLEscapedText = HTMLEscapedText { printEscaped :: T.Text } deriving Show
+
+
+escapeHTMLChar :: Parser T.Text
+escapeHTMLChar = (const "&lt;" <$> char '<')
+                 <|> (const "&gt;" <$> char '>')
+                 <|> (const "&quot;" <$> char '\"')
+                 <|> (const "&amp;" <$> char '&')
+                 <|> (const "&#39;" <$> char '\'')
+                 <|> (T.singleton <$> satisfy (notInClass "<>\"&\'"))
+
+
+escapeHTML :: Parser T.Text
+escapeHTML = ((<>) <$> escapeHTMLChar <*> escapeHTML) <|> (pure "") 
+
+
+escape :: T.Text -> HTMLEscapedText
+escape string_ = HTMLEscapedText $ 
+                     either
+                         (\x -> error $ "Could not escape " <> x)
+                         id  
+                         (parseOnly escapeHTML string_)
---
+++ html-generator/test/HtmlEscapedTextSpec.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+module HtmlEscapedTextSpec (spec) where
+
+import qualified Data.Text as T
+
+import Test.Hspec (it, describe, shouldBe, Spec, Expectation)
+
+import HtmlGenerator.HtmlEscapedText (escape, printEscaped)
+
+
+characterExpectation :: Char -> T.Text -> Expectation
+characterExpectation c expected = (printEscaped . escape . T.singleton $ c) `shouldBe` expected
+
+
+spec :: Spec
+spec = describe "HtmlEscapedText" $ do
+    it "Translates & to &amp;" $ characterExpectation '&' "&amp;"
+    it "Translates < to &lt;" $ characterExpectation '<' "&lt;"
+    it "Translates > to &gt;" $ characterExpectation '>' "&gt;"
+    it "Translates \" to &quot;" $ characterExpectation '\"' "&quot;"
+    it "Translates \' to &#39;" $ characterExpectation '\'' "&#39;"
+    it "Translates e to e" $ characterExpectation 'e' "e"
