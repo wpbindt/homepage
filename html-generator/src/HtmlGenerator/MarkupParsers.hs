69a06754598cc28aeb5e770c41e58f6a8bfc816b
wpbindt <wesselbindt@gmail.com>
2022-08-04 11:02:21 +0200

    Add markup parsers

---
+++ html-generator/src/HtmlGenerator/MarkupParsers.hs
@@ -0,0 +1,55 @@
+import Data.Attoparsec.Text
+import qualified Data.Text as T
+import Control.Applicative
+
+
+data MarkupDocument = Markup [LowerLevelMarkup] deriving Show
+data LowerLevelMarkup = Header Int T.Text 
+                        | PlainText T.Text 
+                        | Bold T.Text
+                        | Code [T.Text]
+                        | List [T.Text]
+                            deriving Show
+
+
+block :: Parser T.Text
+block = T.pack <$> manyTill anyChar (string "\n\n")
+
+
+blocks :: Parser [T.Text]
+blocks = many block
+
+
+countChar :: Char -> Parser Int
+countChar c = length <$> many1 (char c)
+
+
+header :: Parser LowerLevelMarkup
+header = Header <$> (countChar '*') <*> (takeTill isEndOfLine)
+
+
+bold :: Parser LowerLevelMarkup
+bold = Bold . T.pack <$> do
+    char '`'
+    manyTill anyChar (char '`')
+
+
+codeLine :: Parser T.Text
+codeLine = do
+    string "\n>" <|> string "\r>"
+    takeTill isEndOfLine
+
+
+codeBlock :: Parser LowerLevelMarkup
+codeBlock = Code <$> (many1 codeLine)
+
+
+listItem :: Parser T.Text
+listItem = do
+    string "\n-" <|> string "\r-"
+    takeTill isEndOfLine
+
+
+orderedList :: Parser LowerLevelMarkup
+orderedList = List <$> (many1 listItem)
+
