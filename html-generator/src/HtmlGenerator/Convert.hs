3a880eaa84f74e2f9dc439299590653b4397eb43
wpbindt <wesselbindt@gmail.com>
2023-01-10 22:43:21 +0100

    Remove old markup

--- html-generator/src/HtmlGenerator/Convert.hs
+++ html-generator/src/HtmlGenerator/Convert.hs
@@ -2,31 +2,29 @@ module HtmlGenerator.Convert where
 
 import qualified Data.Text as T
 
-import qualified HtmlGenerator.Markup as Markup
-import qualified HtmlGenerator.MarkupParsers as NewMarkup
+import qualified HtmlGenerator.MarkupParsers as Markup
 import qualified HtmlGenerator.Html as Html
 
 
-
-convertMarkupDocument :: NewMarkup.Document -> [Html.Html]
-convertMarkupDocument (NewMarkup.Document paragraphs) = map convertParagraph paragraphs
-
-
-convertParagraph :: NewMarkup.Paragraph -> Html.Html
-convertParagraph (NewMarkup.Paragraph tokens) = Html.p_ $ map convertToken tokens
+convertMarkupDocument :: Markup.Document -> [Html.Html]
+convertMarkupDocument (Markup.Document paragraphs) = map convertParagraph paragraphs
 
 
-convertToken :: NewMarkup.MarkupToken -> Html.Html
+convertParagraph :: Markup.Paragraph -> Html.Html
+convertParagraph (Markup.Paragraph tokens) = Html.p_ $ map convertToken tokens
+
+
+convertToken :: Markup.MarkupToken -> Html.Html
 convertToken token = case token of
-    (NewMarkup.Header w c) -> Html.h_ w c
-    (NewMarkup.OrderedList cs) -> Html.ol_ . map (Html.p_ . pure . Html.plain_) $ cs
-    (NewMarkup.CodeBlock cs) -> Html.code_ . T.unlines $ cs
-    (NewMarkup.NormalText c) -> Html.plain_ (c `T.snoc` '\n')
+    (Markup.Header w c) -> Html.h_ w c
+    (Markup.OrderedList cs) -> Html.ol_ . map (Html.p_ . pure . Html.plain_) $ cs
+    (Markup.CodeBlock cs) -> Html.code_ . T.unlines $ cs
+    (Markup.NormalText c) -> Html.plain_ (c `T.snoc` '\n')
 
 
-convertMarkupToHtml :: Html.Title -> NewMarkup.Document -> Html.Html
+convertMarkupToHtml :: Html.Title -> Markup.Document -> Html.Html
 convertMarkupToHtml title = Html.makeBasicHtml title . convertMarkupDocument
 
 
-convert :: Html.Title -> NewMarkup.Document -> Html.Html
+convert :: Html.Title -> Markup.Document -> Html.Html
 convert title = Html.makeBasicHtml title . convertMarkupDocument
