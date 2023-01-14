e79a7813daf97e9deaccc66ca30d5e989059f807
wpbindt <wesselbindt@gmail.com>
2023-01-14 12:50:11 +0100

    Use Data.Text.IO

--- html-generator/src/HtmlGenerator.hs
+++ html-generator/src/HtmlGenerator.hs
@@ -1,6 +1,7 @@
 module HtmlGenerator (main, convert) where
     
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 import System.FilePath (takeBaseName)
@@ -12,7 +13,10 @@ import qualified HtmlGenerator.MarkupParsers as Parser
 
 
 convert :: Html.Title -> T.Text -> T.Text
-convert title input = Html.render . Convert.convertMarkupToHtml title . Parser.parseMarkup $ (input `T.snoc` '\n')
+convert title input = Html.render
+                      . Convert.convertMarkupToHtml title
+                      . Parser.parseMarkup
+                      $ input `T.snoc` '\n'
 
 
 askPermission :: String -> IO Bool
@@ -23,24 +27,29 @@ askPermission prompt = do
     return (answer == "y")
 
 
-safeWrite :: FilePath -> String -> IO ()
+safeWrite :: FilePath -> T.Text -> IO ()
 safeWrite path contents = do
     fileExists <- doesFileExist path
     doWrite <- if fileExists then askPermission "File exists" else return True
-    if doWrite then writeFile path contents else putStrLn "quitting"
+    if doWrite then TIO.writeFile path contents else putStrLn "quitting"
 
 
-readConvertWrite :: Html.Title -> IO String -> (String -> IO ()) -> IO ()
-readConvertWrite title read' write = T.unpack . convert title <$> (T.pack <$> read') >>= write
+readConvertWrite :: Html.Title -> IO T.Text -> (T.Text -> IO ()) -> IO ()
+readConvertWrite title read' write = convert title <$> read' >>= write
+
+
+takeBaseNameT :: String -> T.Text
+takeBaseNameT = T.pack . takeBaseName
 
 
 main :: IO ()
 main = do
     arguments <- getArgs
     case arguments of
-        [] -> 
-            readConvertWrite "dummy" getContents putStr
-        [inputFile, outputFile] -> 
-            readConvertWrite (T.pack . takeBaseName $ inputFile) (readFile inputFile) (safeWrite outputFile)
+        [inputFile, outputFile] ->
+            readConvertWrite
+                (takeBaseNameT inputFile)
+                (TIO.readFile inputFile)
+                (safeWrite outputFile)
         _ -> 
             putStrLn "Wrong usage"
