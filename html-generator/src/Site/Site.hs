913a321b8142b63fadd657d45b72860dbd3c9254
wpbindt <wesselbindt@gmail.com>
2023-01-29 14:24:11 +0100

    Sites are not functorial

--- html-generator/src/Site/Site.hs
+++ html-generator/src/Site/Site.hs
@@ -4,6 +4,6 @@ module Site.Site where
 import Site.Title
 
 
-data Page a = Page Title a deriving (Functor, Eq, Show)
-data Section a = Section Title [Page a] deriving (Functor, Eq, Show)
-data Site a = Site Title [Section a] deriving (Functor, Eq, Show)
+data Page a = Page Title a deriving (Eq, Show)
+data Section a = Section Title [Page a] deriving (Eq, Show)
+data Site a = Site Title [Section a] deriving (Eq, Show)
